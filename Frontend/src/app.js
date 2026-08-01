52baca68dba46ec1a338d1fa79e3f64d95c11095
Mayooran27 <mayooranthiruchchelvam@gmail.com>
2026-08-02 00:12:43 +0100

    app.js

---
+++ Frontend/src/app.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+import RegistrationForm from './components/RegistrationForm';
+
+function App() {
+  return (
+    <div className="app-container">
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
